6eefb9eaf3c920415f480e6a0623729d66b5c1ba
Sakkar Chowdhury <Sakkhor.Chowdhury@gmail.com>
2024-11-17 13:08:36 +0600

    mock api added to project

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:9000/tasks";
+
+//Mock API URL
+export const API_URL = "https://673991a9a3a36b5a62eefad8.mockapi.io/api/tasks";
+
+//JSON Server URL
+
+// export const API_URL = "http://localhost:9000/tasks";
